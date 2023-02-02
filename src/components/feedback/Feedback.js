f57816dc40f6f8e8b3e81281b583754fd4e72c7e
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-02-02 18:59:36 +0200

    update

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,29 +1,60 @@
 import React from "react";
+import Statistics from "./Statistics";
+
 
 class Feedback extends React.Component {
-    constructor() {
-        super();
-        this.state = {
+     static defaultProps = {
             good: 0,
             neutral: 0,
             bad: 0,
-        }
-    }
+            total: 0,
+            positivePercentage: 0
+     };
+        state = {
+            good: this.props.good,
+            neutral: this.props.neutral,
+            bad: this.props.bad,
+            total: this.props.total,
+            positivePercentage: this.props.positivePercentage,
+        };
+    
     buttonGood = () => {
         this.setState(prevState => ({
         good: prevState.good + 1,
-        }));
+        }
+        ));
+        this.countTotalFeedback();
+        this.countPositiveFeedbackPercentage();
     }
     buttonNeutral = () => {
         this.setState(prevState => ({
-        neutral: prevState.neutral + 1
+        neutral: prevState.neutral + 1,
             }));
+            this.countTotalFeedback();
+            this.countPositiveFeedbackPercentage();
     }
     ButtonBad = () => {
         this.setState(prevState => {
             return { bad: prevState.bad + 1,
+                total: prevState.good + prevState.neutral + prevState.bad
             };
             });
+            this.countTotalFeedback();
+            this.countPositiveFeedbackPercentage();
+    }
+    countTotalFeedback = () => {
+        this.setState(prevState => {
+            return {
+                total: prevState.good + prevState.neutral + prevState.bad
+            }
+        })
+    }
+    countPositiveFeedbackPercentage = () => {
+        this.setState(prevState => {
+            return {
+                positivePercentage: (prevState.good / prevState.total * 100).toFixed(0)
+    }
+        })
     }
 
     render() {
@@ -34,12 +65,13 @@ class Feedback extends React.Component {
                 <button type="button" onClick={this.buttonNeutral}>Neutral:</button>
                 <button type="button" onClick={this.ButtonBad}>Bad:</button>
             </div>
-            <div>
-                <h2>Statistics</h2>
-                <p >Good: {this.state.good}</p>
-                <p >Neutral: {this.state.neutral}</p>
-                <p >Bad: {this.state.bad}</p>
-            </div>
+            <Statistics 
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.state.total} 
+            positivePercentage={this.state.positivePercentage}
+            />
         </div>
             
         )
